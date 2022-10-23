c127b733195de553b295116b7bb3e5125b0caec7
Julian Montes <julianmontesPS4@gmail.com>
2022-10-23 17:34:30 -0500

    Project done.

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
-window.addEventListener('load',()=>{
-    const temperatureDegree= document.querySelector('.temperature-degree'); 
+const codes={
+        0: ['Clear Sky','CLEAR_DAY'],
+        1: ['Mainly clear, partly cloudy, and overcast','CLEAR_DAY'],
+        2: ['Mainly clear, partly cloudy, and overcast','CLEAR_DAY'],
+        3: ['Mainly clear, partly cloudy, and overcast','CLEAR_DAY'],
+        45: ['Fog and depositing rime fog','FOG'],
+        48: ['Fog and depositing rime fog','FOG'],
+        51: ['Drizzle: Light, moderate, and dense intensity','RAIN'],
+        55: ['Drizzle: Light, moderate, and dense intensity','RAIN'],
+        53: ['Drizzle: Light, moderate, and dense intensity','RAIN'],
+        56: ['Freezing Drizzle: Light and dense intensity','RAIN'],
+        57: ['Freezing Drizzle: Light and dense intensity','RAIN'],
+        61: ['Rain: Slight, moderate and heavy intensity','RAIN'],
+        63: ['Rain: Slight, moderate and heavy intensity','RAIN'],
+        65: ['Rain: Slight, moderate and heavy intensity','RAIN'],
+        66: ['Freezing Rain: Light and heavy intensity','RAIN'],
+        67: ['Freezing Rain: Light and heavy intensity','RAIN'],
+        71: ['Snow fall: Slight, moderate, and heavy intensity','SNOW'],
+        73: ['Snow fall: Slight, moderate, and heavy intensity','SNOW'],
+        75: ['Snow fall: Slight, moderate, and heavy intensity','SNOW'],
+        77: ['Snow grains','SNOW'],
+        80: ['Rain showers: Slight, moderate, and violent','RAIN'],
+        81: ['Rain showers: Slight, moderate, and violent','RAIN'],
+        82: ['Rain showers: Slight, moderate, and violent','RAIN'],
+        85: ['Snow showers slight and heavy','RAIN'],
+        86: ['Snow showers slight and heavy','RAIN'],
+        95: ['Thunderstorm: Slight or moderate','RAIN'],
+        96: ['Thunderstorm with slight and heavy hail','RAIN'],
+        99: ['Thunderstorm with slight and heavy hail','RAIN']
+ }
 
+//when the page loads:
+window.addEventListener('load',()=>{
+    const tempDegree= document.querySelector('.temperature-degree'); 
     const degreeType= document.querySelector('#degree-type');
+    const tempDes= document.querySelector('.temperature-description');
+    const timeZone= document.querySelector('.location-timezone');
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
@@ -17,13 +50,26 @@ window.addEventListener('load',()=>{
                     return Response.json();
                 })
                 .then(data=>{
+                    console.log(data);
                     const degree= data.current_weather.temperature;
-                    temperatureDegree.innerHTML= `${degree}`;
+                    tempDegree.textContent= `${degree}`;
+                    degreeType.textContent= 'C';
 
-                    degreeType.innerHTML= 'C'
-                })
+                    const weatherCode= data.current_weather.weathercode;
+                    const description= codes[weatherCode][0];
+                    tempDes.textContent=description + '.';
+
+                    timeZone.textContent= data.timezone;
+                    
+                    setIcons(codes[weatherCode][1],document.querySelector('.icon'))
+                });
         });
-
+    }
+    //set Skycons
+    const setIcons=(icon, iconID)=>{
+        const skycons= new Skycons({color:'white'});
+        skycons.play();
+        return skycons.set(iconID, Skycons[icon])
     }
 })
 
